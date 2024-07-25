bd952e80d6fa2846cdf8d1e856346683350e5c47
jiyun03 <sprare06@gmail.com>
2024-07-26 01:58:49 +0900

    메타데이터 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 import Header from '@/components/common/Header'
 import { ThemeProvider } from '@/context/themeProvider'
@@ -26,6 +27,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <GlobalStyle />
+      <Head>
+        <title>박지윤 포트폴리오</title>
+        <meta property="og:title" content="박지윤 포트폴리오" />
+        <meta property="og:description" content="박지윤의 포트폴리오 사이트입니다." />
+        <meta property="og:image" content="/assets/img/common/menu_point.jpg" />
+        <meta property="og:url" content="https://sprare06.vercel.app" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </ThemeProvider>
